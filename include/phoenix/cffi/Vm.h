765e0a6daf77427f2770d2e7aaddba36bc3fe1e3
Luis Michaelis <me@lmichaelis.de>
2023-04-04 21:14:47 +0200

    feat(vm): add API for instances

--- include/phoenix/cffi/Vm.h
+++ include/phoenix/cffi/Vm.h
@@ -8,6 +8,12 @@
 #include <stdint.h>
 
 typedef struct PxInternal_Vm PxVm;
+typedef struct PxInternal_VmInstance PxVmInstance;
+
+typedef enum {
+	PxVmInstanceTypeNpc = 1,
+} PxVmInstanceType;
+
 typedef void (*PxVmExternalCallback)(PxVm* vm);
 typedef void (*PxVmExternalDefaultCallback)(PxVm* vm, char const* name);
 
@@ -16,9 +22,22 @@ PXC_API PxVm* pxVmLoad(PxBuffer* buffer);
 PXC_API PxVm* pxVmLoadFromVdf(PxVdf const* vdf, char const* name);
 PXC_API void pxVmDestroy(PxVm* vm);
 
-PXC_API char const* pxVmPopString(PxVm* vm);
-PXC_API float pxVmPopFloat(PxVm* vm);
-PXC_API int32_t pxVmPopInt(PxVm* vm);
+PXC_API PxVmInstance* pxVmStackPopInstance(PxVm* vm);
+PXC_API char const* pxVmStackPopString(PxVm* vm);
+PXC_API float pxVmStackPopFloat(PxVm* vm);
+PXC_API int32_t pxVmStackPopInt(PxVm* vm);
+
+PXC_API void pxVmStackPushInstance(PxVm* vm, PxVmInstance* instance);
+PXC_API void pxVmStackPushString(PxVm* vm, char const* string);
+PXC_API void pxVmStackPushFloat(PxVm* vm, float f);
+PXC_API void pxVmStackPushInt(PxVm* vm, int i);
 
 PXC_API void pxVmRegisterExternal(PxVm* vm, char const* name, PxVmExternalCallback cb);
 PXC_API void pxVmRegisterExternalDefault(PxVm* vm, PxVmExternalDefaultCallback cb);
+
+PXC_API PxVmInstance* pxVmInstanceAllocate(PxVm* vm, char const* name, PxVmInstanceType type);
+PXC_API PxVmInstance* pxVmInstanceInitialize(PxVm* vm, char const* name, PxVmInstanceType type, PxVmInstance* existing);
+
+PXC_API uint32_t pxVmInstanceNpcGetNameLength(PxVmInstance const* instance);
+PXC_API char const* pxVmInstanceNpcGetName(PxVmInstance const* instance, uint32_t i);
+PXC_API int32_t pxVmInstanceNpcGetRoutine(PxVmInstance const* instance);
--- src/Vm.cc
+++ src/Vm.cc
@@ -1,9 +1,13 @@
 // Copyright © 2023. GothicKit Contributors
 // SPDX-License-Identifier: MIT
+#include "Prelude.h"
+
 #include <phoenix/cffi/Vm.h>
 #include <phoenix/ext/daedalus_classes.hh>
 #include <phoenix/vm.hh>
 
+#include <memory>
+#include <stdexcept>
 #include <unordered_map>
 
 struct PxInternal_Vm {
@@ -56,16 +60,38 @@ void pxVmDestroy(PxVm* vm) {
 	delete vm;
 }
 
-char const* pxVmPopString(PxVm* vm) {
+PxVmInstance* pxVmStackPopInstance(PxVm* vm) {
+	return RC(PxVmInstance, vm->vm.pop_instance().get());
+}
+
+char const* pxVmStackPopString(PxVm* vm) {
 	return vm->vm.pop_string().c_str();
 }
 
-float pxVmPopFloat(PxVm* vm) {
+float pxVmStackPopFloat(PxVm* vm) {
 	return vm->vm.pop_float();
 }
 
-int32_t pxVmPopInt(PxVm* vm) {
+int32_t pxVmStackPopInt(PxVm* vm) {
 	return vm->vm.pop_int();
+}
+
+void pxVmStackPushInstance(PxVm* vm, PxVmInstance* instance) {
+	auto* inst = RC(px::instance, instance);
+	auto* sym = vm->vm.find_symbol_by_index(inst->symbol_index());
+	vm->vm.push_instance(sym->get_instance());
+}
+
+void pxVmStackPushString(PxVm* vm, char const* string) {
+	vm->vm.push_string(string);
+}
+
+void pxVmStackPushFloat(PxVm* vm, float f) {
+	vm->vm.push_float(f);
+}
+
+void pxVmStackPushInt(PxVm* vm, int i) {
+	vm->vm.push_int(i);
 }
 
 void pxVmRegisterExternal(PxVm* vm, char const* name, PxVmExternalCallback cb) {
@@ -76,4 +102,54 @@ void pxVmRegisterExternal(PxVm* vm, char const* name, PxVmExternalCallback cb) {
 
 void pxVmRegisterExternalDefault(PxVm* vm, PxVmExternalDefaultCallback cb) {
 	vm->defaultExternal = cb;
+}
+
+PxVmInstance* pxVmInstanceAllocate(PxVm* vm, char const* name, PxVmInstanceType type) {
+	try {
+		auto* sym = vm->vm.find_symbol_by_name(name);
+		if (sym == nullptr) return nullptr;
+
+		phoenix::instance* instance = nullptr;
+		switch (type) {
+		case PxVmInstanceTypeNpc:
+			instance = vm->vm.allocate_instance<phoenix::c_npc>(sym).get();
+			break;
+		}
+
+		return RC(PxVmInstance, instance);
+	} catch (std::runtime_error const& exc) {
+		return nullptr;
+	}
+}
+
+PxVmInstance* pxVmInstanceInitialize(PxVm* vm, char const* name, PxVmInstanceType type, PxVmInstance* existing) {
+	if (existing == nullptr) existing = pxVmInstanceAllocate(vm, name, type);
+	if (existing == nullptr) return nullptr;
+
+	try {
+		auto* sym = vm->vm.find_symbol_by_name(name);
+		if (sym == nullptr) return nullptr;
+
+		switch (type) {
+		case PxVmInstanceTypeNpc:
+			vm->vm.init_instance<phoenix::c_npc>(std::reinterpret_pointer_cast<px::c_npc>(sym->get_instance()), sym);
+			break;
+		}
+
+		return existing;
+	} catch (std::runtime_error const& exc) {
+		return nullptr;
+	}
+}
+
+uint32_t pxVmInstanceNpcGetNameLength(PxVmInstance const* instance) {
+	return RCC(phoenix::c_npc, instance)->name_count;
+}
+
+char const* pxVmInstanceNpcGetName(PxVmInstance const* instance, uint32_t i) {
+	return RCC(phoenix::c_npc, instance)->name[i].c_str();
+}
+
+int32_t pxVmInstanceNpcGetRoutine(PxVmInstance const* instance) {
+	return RCC(phoenix::c_npc, instance)->daily_routine;
 }
