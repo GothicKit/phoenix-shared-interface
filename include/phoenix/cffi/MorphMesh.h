1bb8a07ca4db1b5117851615b8e744933cd30e43
Luis Michaelis <me@lmichaelis.de>
2023-04-05 13:07:28 +0200

    feat(MorphMesh): add more apis

--- include/phoenix/cffi/MorphMesh.h
+++ include/phoenix/cffi/MorphMesh.h
@@ -3,13 +3,35 @@
 #pragma once
 #include "Api.h"
 #include "Buffer.h"
+#include "MultiResolutionMesh.h"
 #include "Vdf.h"
 
 #include <stdint.h>
 
-typedef struct s_PxMorphMesh PxMorphMesh;
+typedef struct PxInternal_MorphMesh PxMorphMesh;
+typedef struct PxInternal_MorphMeshAnimation PxMorphMeshAnimation;
 
 // TODO
 PXC_API PxMorphMesh* pxMmbLoad(PxBuffer* buffer);
 PXC_API PxMorphMesh* pxMmbLoadFromVdf(PxVdf const* vdf, char const* name);
 PXC_API void pxMmbDestroy(PxMorphMesh* mmb);
+
+PXC_API char const* pxMmbGetName(PxMorphMesh const* mmb);
+PXC_API PxMultiResolutionMesh const* pxMmbGetMesh(PxMorphMesh const* mmb);
+PXC_API uint32_t pxMmbGetMorphPositionCount(PxMorphMesh const* mmb);
+PXC_API PxVec3 pxMmbGetMorphPosition(PxMorphMesh const* mmb, uint32_t i);
+PXC_API uint32_t pxMmbGetAnimationCount(PxMorphMesh const* mmb);
+PXC_API PxMorphMeshAnimation const* pxMmbGetAnimation(PxMorphMesh const* mmb, uint32_t i);
+// TODO: Sources
+
+PXC_API char const* pxMmbAniGetName(PxMorphMeshAnimation const* ani);
+PXC_API int32_t pxMmbAniGetLayer(PxMorphMeshAnimation const* ani);
+PXC_API float pxMmbAniGetBlendIn(PxMorphMeshAnimation const* ani);
+PXC_API float pxMmbAniGetBlendOut(PxMorphMeshAnimation const* ani);
+PXC_API float pxMmbAniGetDuration(PxMorphMeshAnimation const* ani);
+PXC_API float pxMmbAniGetSpeed(PxMorphMeshAnimation const* ani);
+PXC_API uint8_t pxMmbAniGetFlags(PxMorphMeshAnimation const* ani);
+PXC_API uint32_t pxMmbAniGetFrameCount(PxMorphMeshAnimation const* ani);
+PXC_API uint32_t const* pxMmbAniGetVertices(PxMorphMeshAnimation const* ani, uint32_t* length);
+PXC_API uint32_t pxMmbAniGetSampleCount(PxMorphMeshAnimation const* ani);
+PXC_API PxVec3 pxMmbAniGetSample(PxMorphMeshAnimation const* ani, uint32_t i);
--- src/MorphMesh.cc
+++ src/MorphMesh.cc
@@ -28,3 +28,75 @@ PxMorphMesh* pxMmbLoadFromVdf(PxVdf const* vdf, char const* name) {
 void pxMmbDestroy(PxMorphMesh* mmb) {
 	delete reinterpret_cast<phoenix::morph_mesh*>(mmb);
 }
+
+char const* pxMmbGetName(PxMorphMesh const* mmb) {
+	return RCC(px::morph_mesh, mmb)->name.c_str();
+}
+
+PxMultiResolutionMesh const* pxMmbGetMesh(PxMorphMesh const* mmb) {
+	return RCC(PxMultiResolutionMesh, &RCC(px::morph_mesh, mmb)->mesh);
+}
+
+uint32_t pxMmbGetMorphPositionCount(PxMorphMesh const* mmb) {
+	return (uint32_t) RCC(px::morph_mesh, mmb)->morph_positions.size();
+}
+
+PxVec3 pxMmbGetMorphPosition(PxMorphMesh const* mmb, uint32_t i) {
+	auto& pos = RCC(px::morph_mesh, mmb)->morph_positions[i];
+	return PxVec3 {pos.x, pos.y, pos.z};
+}
+
+uint32_t pxMmbGetAnimationCount(PxMorphMesh const* mmb) {
+	return (uint32_t) RCC(px::morph_mesh, mmb)->animations.size();
+}
+
+PxMorphMeshAnimation const* pxMmbGetAnimation(PxMorphMesh const* mmb, uint32_t i) {
+	return RCC(PxMorphMeshAnimation, &RCC(px::morph_mesh, mmb)->animations[i]);
+}
+
+char const* pxMmbAniGetName(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->name.c_str();
+}
+
+int32_t pxMmbAniGetLayer(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->layer;
+}
+
+float pxMmbAniGetBlendIn(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->blend_in;
+}
+
+float pxMmbAniGetBlendOut(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->blend_out;
+}
+
+float pxMmbAniGetDuration(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->duration;
+}
+
+float pxMmbAniGetSpeed(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->duration;
+}
+
+uint8_t pxMmbAniGetFlags(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->flags;
+}
+
+uint32_t pxMmbAniGetFrameCount(PxMorphMeshAnimation const* ani) {
+	return RCC(px::morph_animation, ani)->frame_count;
+}
+
+uint32_t const* pxMmbAniGetVertices(PxMorphMeshAnimation const* ani, uint32_t* length) {
+	auto& verts = RCC(px::morph_animation, ani)->vertices;
+	*length = (uint32_t) verts.size();
+	return RCC(px::morph_animation, ani)->vertices.data();
+}
+
+uint32_t pxMmbAniGetSampleCount(PxMorphMeshAnimation const* ani) {
+	return (uint32_t) RCC(px::morph_animation, ani)->samples.size();
+}
+
+PxVec3 pxMmbAniGetSample(PxMorphMeshAnimation const* ani, uint32_t i) {
+	auto& pos = RCC(px::morph_animation, ani)->samples[i];
+	return PxVec3 {pos.x, pos.y, pos.z};
+}
