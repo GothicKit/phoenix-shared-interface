8f7359710cf292fe8d09edec4fd77d1820d6738a
Luis Michaelis <me@lmichaelis.de>
2023-04-05 12:32:38 +0200

    feat(ModelMesh): add getters

--- include/phoenix/cffi/ModelMesh.h
+++ include/phoenix/cffi/ModelMesh.h
@@ -4,12 +4,27 @@
 #include "Api.h"
 #include "Buffer.h"
 #include "Vdf.h"
+#include "MultiResolutionMesh.h"
 
 #include <stdint.h>
 
 typedef struct PxInternal_ModelMesh PxModelMesh;
+typedef struct PxInternal_SoftSkinMesh PxSoftSkinMesh;
 
-// TODO
 PXC_API PxModelMesh* pxMdmLoad(PxBuffer* buffer);
 PXC_API PxModelMesh* pxMdmLoadFromVdf(PxVdf const* vdf, char const* name);
 PXC_API void pxMdmDestroy(PxModelMesh* mdm);
+
+PXC_API uint32_t pxMdmGetMeshCount(PxModelMesh const* mdm);
+PXC_API PxSoftSkinMesh const* pxMdmGetMesh(PxModelMesh const* mdm, uint32_t i);
+PXC_API PxMultiResolutionMesh const* pxMdmGetAttachment(PxModelMesh const* mdm, char const* name);
+PXC_API uint32_t pxMdmGetChecksum(PxModelMesh const* mdm);
+
+PXC_API PxMultiResolutionMesh const* pxSsmGetMesh(PxSoftSkinMesh const* ssm);
+// TODO: bboxes
+PXC_API uint32_t pxSsmGetWedgeNormalsCount(PxSoftSkinMesh const* ssm);
+PXC_API void pxSsmGetWedgeNormal(PxSoftSkinMesh const* ssm, uint32_t i, PxVec3* normal, uint32_t* index);
+PXC_API uint32_t pxSsmGetNodeCount(PxSoftSkinMesh const* ssm);
+PXC_API uint32_t pxSsmGetNodeWeightCount(PxSoftSkinMesh const* ssm, uint32_t node);
+PXC_API void pxSsmGetNodeWeight(PxSoftSkinMesh const* ssm, uint32_t node, uint32_t i, float* weight, PxVec3* position, uint8_t* index);
+PXC_API int32_t const* pxSsmGetNodes(PxSoftSkinMesh const* ssm, uint32_t* length);
--- src/ModelMesh.cc
+++ src/ModelMesh.cc
@@ -28,3 +28,62 @@ PxModelMesh* pxMdmLoadFromVdf(PxVdf const* vdf, char const* name) {
 void pxMdmDestroy(PxModelMesh* mdm) {
 	delete reinterpret_cast<phoenix::model_mesh*>(mdm);
 }
+
+uint32_t pxMdmGetMeshCount(PxModelMesh const* mdm) {
+    return (uint32_t) RCC(px::model_mesh, mdm)->meshes.size();
+}
+
+PxSoftSkinMesh const* pxMdmGetMesh(PxModelMesh const* mdm, uint32_t i) {
+	return RCC(PxSoftSkinMesh, &RCC(px::model_mesh, mdm)->meshes[i]);
+}
+
+PxMultiResolutionMesh const* pxMdmGetAttachment(PxModelMesh const* mdm, char const* name) {
+	auto& attachments = RCC(px::model_mesh, mdm)->attachments;
+
+	auto rv = attachments.find(name);
+	if (rv == attachments.end()) return nullptr;
+
+	return RCC(PxMultiResolutionMesh, &rv->second);
+}
+
+uint32_t pxMdmGetChecksum(PxModelMesh const* mdm) {
+	return RCC(px::model_mesh, mdm)->checksum;
+}
+
+PxMultiResolutionMesh const* pxSsmGetMesh(PxSoftSkinMesh const* ssm) {
+	return RCC(PxMultiResolutionMesh, &RCC(px::softskin_mesh, ssm)->mesh);
+}
+
+uint32_t pxSsmGetWedgeNormalsCount(PxSoftSkinMesh const* ssm) {
+	return (uint32_t) RCC(px::softskin_mesh, ssm)->wedge_normals.size();
+}
+
+void pxSsmGetWedgeNormal(PxSoftSkinMesh const* ssm, uint32_t i, PxVec3* normal, uint32_t* index) {
+	auto& wg = RCC(px::softskin_mesh, ssm)->wedge_normals[i];
+	normal->x = wg.normal.x;
+	normal->y = wg.normal.y;
+	normal->z = wg.normal.z;
+	*index = wg.index;
+}
+
+uint32_t pxSsmGetNodeCount(PxSoftSkinMesh const* ssm) {
+	return (uint32_t) RCC(px::softskin_mesh, ssm)->weights.size();
+}
+
+uint32_t pxSsmGetNodeWeightCount(PxSoftSkinMesh const* ssm, uint32_t node) {
+	return (uint32_t) RCC(px::softskin_mesh, ssm)->weights[node].size();
+}
+
+void pxSsmGetNodeWeight(PxSoftSkinMesh const* ssm, uint32_t node, uint32_t i, float* weight, PxVec3* position, uint8_t* index) {
+	auto& w = RCC(px::softskin_mesh, ssm)->weights[node][i];
+	*weight = w.weight;
+	*index = w.node_index;
+	position->x = w.position.x;
+	position->y = w.position.y;
+	position->z = w.position.z;
+}
+
+int32_t const* pxSsmGetNodes(PxSoftSkinMesh const* ssm, uint32_t* length) {
+	*length = (uint32_t) RCC(px::softskin_mesh, ssm)->nodes.size();
+	return RCC(px::softskin_mesh, ssm)->nodes.data();
+}
