cd540f28bbc420c578d03773351aa939d46fe926
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-21 13:37:24 +0200

    Initial commit of MeshScript functions.

--- include/phoenix/cffi/ModelScript.h
+++ include/phoenix/cffi/ModelScript.h
@@ -14,7 +14,61 @@ typedef phoenix::model_script PxModelScript;
 typedef struct PxInternal_ModelScript PxModelScript;
 #endif
 
-// TODO
+typedef enum : uint8_t {
+	af_none = 0,
+	af_move = 1,
+	af_rotate = 2,
+	af_queue = 4,
+	af_fly = 8,
+	af_idle = 16,   
+} PxAnimationFlags;
+
+typedef enum : uint8_t {
+	forward = 0,  ///< The animation samples are played from first to last.
+	backward = 1, ///< The animation samples are played from last to first.
+} PxAnimationDirection;
+
+
 PXC_API PxModelScript* pxMdsLoad(PxBuffer* buffer);
 PXC_API PxModelScript* pxMdsLoadFromVdf(PxVdf const* vdf, char const* name);
-PXC_API void pxMdsDestroy(PxModelScript* mdm);
+PXC_API void pxMdsDestroy(PxModelScript* mds);
+
+PXC_API uint32_t pxMdsGetMeshCount(PxModelScript* mds);
+PXC_API const char* pxMdsGetMesh(PxModelScript* mds, uint32_t i);
+PXC_API uint32_t pxMdsGetDisabledAnimationsCount(PxModelScript* mds);
+PXC_API const char* pxMdsGetDisabledAnimation(PxModelScript* mds, uint32_t i);
+PXC_API uint32_t pxMdsGetModelTagCount(PxModelScript* mds);
+PXC_API void pxMdsGetModelTag(PxModelScript* mds, uint32_t i, const char* bone);
+
+PXC_API uint32_t pxMdsGetAnimationCombinationCount(PxModelScript* mds);
+PXC_API void pxMdsGetAnimationCombination(PxModelScript* mds, uint32_t i,
+	const char* name,
+    uint32_t* layer,
+    const char* next,
+	float* blend_in,
+	float* blend_out,
+	PxAnimationFlags* flags,
+	const char* model,
+	int32_t* last_frame);
+
+PXC_API uint32_t pxMdsGetAnimationBlendingCount(PxModelScript* mds);
+PXC_API void pxMdsGetAnimationBlending(PxModelScript* mds, uint32_t i,
+	const char* name,
+	const char* next,
+	float* blend_in,
+	float* blend_out);
+
+PXC_API uint32_t pxMdsGetAnimationAliasCount(PxModelScript* mds);
+PXC_API void pxMdsGetAnimationAlias(PxModelScript* mds, uint32_t i,
+			const char* name,
+			uint32_t* layer,
+			const char* next,
+			float* blend_in,
+			float* blend_out,
+			PxAnimationFlags* flags,
+			const char* alias,
+			PxAnimationDirection* direction);
+
+// TODO - Implement
+// mds::skeleton skeleton {};
+// std::vector<mds::animation> animations {};
--- src/ModelScript.cc
+++ src/ModelScript.cc
@@ -31,3 +31,109 @@ PxModelScript* pxMdsLoadFromVdf(PxVdf const* vdf, char const* name) {
 void pxMdsDestroy(PxModelScript* mdm) {
 	delete mdm;
 }
+
+
+
+uint32_t pxMdsGetMeshCount(PxModelScript* mds)
+{
+	return mds->meshes.size();
+}
+
+const char* pxMdsGetMesh(PxModelScript* mds, uint32_t i)
+{
+	return mds->meshes[i].c_str();
+}
+
+uint32_t pxMdsGetDisabledAnimationsCount(PxModelScript* mds)
+{
+	return mds->disabled_animations.size();
+}
+
+const char* pxMdsGetDisabledAnimation(PxModelScript* mds, uint32_t i)
+{
+	return mds->disabled_animations[i].c_str();
+}
+
+uint32_t pxMdsGetModelTagCount(PxModelScript* mds)
+{
+	return mds->model_tags.size();
+}
+
+void pxMdsGetModelTag(PxModelScript* mds, uint32_t i, const char* bone)
+{
+	auto &tag = mds->model_tags[i];
+	bone = tag.bone.c_str();
+}
+
+uint32_t pxMdsGetAnimationCombinationCount(PxModelScript* mds)
+{
+	return mds->combinations.size();
+}
+
+void pxMdsGetAnimationCombination(PxModelScript* mds, uint32_t i,
+	const char* name,
+    uint32_t* layer,
+    const char* next,
+	float* blend_in,
+	float* blend_out,
+	PxAnimationFlags* flags,
+	const char* model,
+	int32_t* last_frame)
+{
+	auto &combination = mds->combinations[i];
+
+	name = combination.name.c_str(),
+    *layer = combination.layer,
+    next = combination.next.c_str(),
+	*blend_in = combination.blend_in,
+	*blend_out = combination.blend_out,
+	*flags = static_cast<PxAnimationFlags>(combination.flags),
+	model = combination.model.c_str(),
+	*last_frame = combination.last_frame;
+}
+
+uint32_t pxMdsGetAnimationBlendingCount(PxModelScript* mds)
+{
+	return mds->blends.size();
+}
+
+void pxMdsGetAnimationBlending(PxModelScript* mds, uint32_t i,
+	const char* name,
+	const char* next,
+	float* blend_in,
+	float* blend_out)
+{
+	auto &blending = mds->blends[i];
+
+	name = blending.name.c_str();
+	next = blending.next.c_str();
+	*blend_in = blending.blend_in;
+	*blend_out = blending.blend_out;
+}
+
+uint32_t pxMdsGetAnimationAliasCount(PxModelScript* mds)
+{
+	return mds->aliases.size();
+}
+
+void pxMdsGetAnimationAlias(PxModelScript* mds, uint32_t i,
+			const char* name,
+			uint32_t* layer,
+			const char* next,
+			float* blend_in,
+			float* blend_out,
+			PxAnimationFlags* flags,
+			const char* alias,
+			PxAnimationDirection* direction)
+{
+	auto &animAlias = mds->aliases[i];
+
+	name = animAlias.name.c_str();
+	*layer = animAlias.layer;
+	next = animAlias.next.c_str();
+	*blend_in = animAlias.blend_in;
+	*blend_out = animAlias.blend_out;
+	*flags = static_cast<PxAnimationFlags>(animAlias.flags);
+	alias = animAlias.alias.c_str();
+	*direction = static_cast<PxAnimationDirection>(animAlias.direction);
+}
