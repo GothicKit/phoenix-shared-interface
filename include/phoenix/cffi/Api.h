51211edb73388e6de9fff2168d7042dc3eedfe09
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-04-26 20:21:43 +0200

    reverted to put ModelHierarchy topics into another PR.

--- include/phoenix/cffi/Api.h
+++ include/phoenix/cffi/Api.h
@@ -55,25 +55,6 @@ typedef struct {
 } PxMat3x3;
 
 typedef struct {
-	float m00;
-	float m01;
-	float m02;
-	float m03;
-	float m10;
-	float m11;
-	float m12;
-	float m13;
-	float m20;
-	float m21;
-	float m22;
-	float m23;
-	float m30;
-	float m31;
-	float m32;
-	float m33;
-} PxMat4x4;
-
-typedef struct {
 	PxVec3 min, max;
 } PxAABB;
 
