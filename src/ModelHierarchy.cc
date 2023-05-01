3f1655c9331b5ec4528deb87ce25b6518bf2720e
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-05-01 13:10:02 +0200

    feat(PxModelHierarchy): add `transform` to `pxMdhGetNode`
    Co-authored-by: Luis Michaelis <me@lmichaelis.de>

--- src/ModelHierarchy.cc
+++ src/ModelHierarchy.cc
@@ -61,11 +61,24 @@ uint32_t pxMdhGetNodeCount(PxModelHierarchy const* mdh) {
 	return (uint32_t) mdh->nodes.size();
 }
 
-void pxMdhGetNode(PxModelHierarchy const* mdh,
-                  uint32_t i,
-                  int16_t* parent,
-                  char const** name /*, TODO: Node transform*/) {
+void pxMdhGetNode(PxModelHierarchy const* mdh, uint32_t i, int16_t* parent, char const** name, PxMat4x4* transform) {
 	auto& node = mdh->nodes[i];
 	*parent = node.parent_index;
 	*name = node.name.c_str();
+	transform->m00 = node.transform[0][0];
+	transform->m01 = node.transform[0][1];
+	transform->m02 = node.transform[0][2];
+	transform->m03 = node.transform[0][3];
+	transform->m10 = node.transform[1][0];
+	transform->m11 = node.transform[1][1];
+	transform->m12 = node.transform[1][2];
+	transform->m13 = node.transform[1][3];
+	transform->m20 = node.transform[2][0];
+	transform->m21 = node.transform[2][1];
+	transform->m22 = node.transform[2][2];
+	transform->m23 = node.transform[2][3];
+	transform->m30 = node.transform[3][0];
+	transform->m31 = node.transform[3][1];
+	transform->m32 = node.transform[3][2];
+	transform->m33 = node.transform[3][3];
 }
