80b85828d3b0b754a6b1f45e557a015506cd27b4
Jucan Daniel <setrofex@gmail.com>
2023-06-07 15:18:28 +0300

    [#22] feat(PxBuffer): add buffer length and array functions

--- include/phoenix/cffi/Buffer.h
+++ include/phoenix/cffi/Buffer.h
@@ -15,3 +15,5 @@ typedef struct PxInternal_Buffer PxBuffer;
 PXC_API PxBuffer* pxBufferCreate(uint8_t const* bytes, uint64_t size);
 PXC_API PxBuffer* pxBufferMmap(char const* file);
 PXC_API void pxBufferDestroy(PxBuffer* buffer);
+PXC_API uint64_t pxBufferSize(PxBuffer* buffer);
+PXC_API std::byte* pxBufferArray(PxBuffer* buffer);
