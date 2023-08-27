b5d43c3d83b9277fdb66336fad2ac28ef0a57648
JaXt0r <120568393+JaXt0r@users.noreply.github.com>
2023-08-27 17:21:25 +0200

    Get all mdm attachment keys via iterator.

--- src/ModelMesh.cc
+++ src/ModelMesh.cc
@@ -40,11 +40,28 @@ PxSoftSkinMesh const* pxMdmGetMesh(PxModelMesh const* mdm, uint32_t i) {
 	return &mdm->meshes[i];
 }
 
-PxMultiResolutionMesh const* pxMdmGetAttachment(PxModelMesh const* mdm, char const* name) {
-	auto& attachments = mdm->attachments;
+uint32_t pxMdmGetAttachmentCount(PxModelMesh const* mdm) {
+	return mdm->attachments.size();
+}
 
+char const* pxMdmGetAttachmentKey(PxModelMesh const* mdm, uint32_t index) {
+	auto& attachments = mdm->attachments;
+    uint32_t currentIndex = 0;
+
+	for (auto i = attachments.begin(); i != attachments.end(); i++) {
+		if (currentIndex++ == index)
+			return i->first.c_str();
+	}
+
+	return nullptr;
+}
+
+PxMultiResolutionMesh const* pxMdmGetAttachmentValue(PxModelMesh const* mdm, char const* name) {
+	auto& attachments = mdm->attachments;
 	auto rv = attachments.find(name);
-	if (rv == attachments.end()) return nullptr;
+
+	if (rv == attachments.end())
+		return nullptr;
 
 	return &rv->second;
 }
